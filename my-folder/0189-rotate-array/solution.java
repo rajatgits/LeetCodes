47582f5a4239d785273fb785da1bf4153c79195f
Rajat Raj <rajatrajemails@gmail.com>
2025-01-17 12:36:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 57.5 MB (47.85%)

---
+++ my-folder/0189-rotate-array/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        rotateArray(nums, 0, nums.length - 1);
+        rotateArray(nums, 0, k - 1);
+        rotateArray(nums, k, nums.length - 1);
+    }
+
+    private void rotateArray(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
