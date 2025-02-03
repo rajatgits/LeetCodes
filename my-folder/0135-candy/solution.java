cf04e7fdde8dd6e51c4886b7f7679c3b18281d9e
Rajat Raj <rajatrajemails@gmail.com>
2025-02-03 14:11:36 +0000

    [LeetCode Sync] Runtime - 3 ms (83.14%), Memory - 45.9 MB (50.34%)

---
+++ my-folder/0135-candy/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int sum = 0;
+        int[] candies = new int[ratings.length];
+
+        Arrays.fill(candies, 1);
+
+        for(int i = 0; i < ratings.length - 1; i++) {
+            if(ratings[i + 1] > ratings[i]) {
+                candies[i+1] = candies[i] + 1;
+            }
+        }
+
+        for(int i = ratings.length - 1; i > 0; i--) {
+            if(ratings[i - 1] > ratings[i]) {
+                if(candies[i - 1] < (candies[i] + 1)) {
+                    candies[i - 1] = candies[i] + 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < candies.length; i++) {
+            sum += candies[i];
+        }
+
+        return sum;
+    }
+}
