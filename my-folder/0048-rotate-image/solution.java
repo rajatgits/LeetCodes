c193ea8b5fc10f3125af065e34fc40d8bf496f39
Rajat Raj <rajatrajemails@gmail.com>
2025-02-04 16:19:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.7 MB (7.94%)

---
+++ my-folder/0048-rotate-image/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        for(int i = 0; i < matrix.length / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[matrix.length - i - 1];
+            matrix[matrix.length - i - 1] = temp;
+        }
+        
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+    }
+}
