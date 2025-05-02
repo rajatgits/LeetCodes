1d2c2c4d0e1700145af672780c010f0dcab96df4
Rajat Raj <rajatrajemails@gmail.com>
2025-05-02 13:14:14 +0000

    [LeetCode Sync] Runtime - 13 ms (75.99%), Memory - 48.8 MB (44.89%)

--- my-folder/0347-top-k-frequent-elements/solution.java
+++ my-folder/0347-top-k-frequent-elements/solution.java
@@ -6,27 +6,21 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer>[] arr = new List[nums.length + 1];
-
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = new ArrayList<>();
-        }
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
         for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            arr[entry.getValue()].add(entry.getKey());
-        }
-
-        int[] ans = new int[k];
-        int index = 0;
-
-        for(int i = arr.length - 1; i > 0 && index < k; i--) {
-            for(int n : arr[i]) {
-                ans[index++] = n;
-                if(index == k) {
-                    return ans;
-                }
+            minHeap.offer(entry);
+            if(minHeap.size() > k) {
+                minHeap.poll();
             }
         }
-        return ans;
+
+        int[] arr = new int[k];
+        int i = 0;
+
+        while(!minHeap.isEmpty()) {
+            arr[i++] = minHeap.poll().getKey();
+        }
+        return arr;
     }
 }
