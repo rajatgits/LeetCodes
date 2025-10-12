a72e7a8110e2150ddbb07c3d752f71d04df144b5
Rajat Raj <rajatrajemails@gmail.com>
2025-10-12 08:30:02 +0000

    [LeetCode Sync] Runtime - 7 ms (100.00%), Memory - 49.5 MB (58.70%)

--- my-folder/0303-range-sum-query---immutable/solution.java
+++ my-folder/0303-range-sum-query---immutable/solution.java
@@ -3,7 +3,7 @@ class NumArray {
     public NumArray(int[] nums) {
         this.nums = nums;
         for(int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i-1];
+            nums[i] = nums[i - 1] + nums[i];
         }
     }
     
@@ -11,6 +11,7 @@ class NumArray {
         if(left == 0) {
             return nums[right];
         }
+
         return nums[right] - nums[left - 1];
     }
 }
