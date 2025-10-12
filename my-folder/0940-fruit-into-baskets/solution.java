47f566a6a4177bdc976c28c03e40ae467f9c9f27
Rajat Raj <rajatrajemails@gmail.com>
2025-10-12 12:40:27 +0000

    [LeetCode Sync] Runtime - 51 ms (41.71%), Memory - 55 MB (88.34%)

--- my-folder/0940-fruit-into-baskets/solution.java
+++ my-folder/0940-fruit-into-baskets/solution.java
@@ -1,23 +1,21 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int l = 0;
-        int r = 0;
+        int left = 0;
         int maxLen = 0;
 
         Map<Integer, Integer> map = new HashMap<>();
 
-        while(r < fruits.length) {
-            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
+        for(int right = 0; right < fruits.length; right++) {
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
 
             if(map.size() > 2) {
-                map.put(fruits[l], map.get(fruits[l]) - 1);
-                if(map.get(fruits[l]) == 0) {
-                    map.remove(fruits[l]);
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+                if(map.get(fruits[left]) == 0) {
+                    map.remove(fruits[left]);
                 }
-                l += 1;
+                left++;
             }
-            maxLen = Math.max(maxLen, r - l + 1);
-            r += 1;
+            maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;
     }
