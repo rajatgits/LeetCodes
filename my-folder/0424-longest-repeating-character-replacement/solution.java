379e6e61a00986d5bbd2b3a48113d3c39c5aa4eb
Rajat Raj <rajatrajemails@gmail.com>
2025-10-13 15:55:59 +0000

    [LeetCode Sync] Runtime - 40 ms (11.11%), Memory - 45.3 MB (24.68%)

--- my-folder/0424-longest-repeating-character-replacement/solution.java
+++ my-folder/0424-longest-repeating-character-replacement/solution.java
@@ -1,22 +1,19 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int left = 0;
-        int right = 0;
+        int count = 0;
         int maxLen = 0;
-        int maxFreq = 0;
-
         Map<Character, Integer> map = new HashMap<>();
 
-        while(right < s.length()) {
+        for(int right = 0; right < s.length(); right++) {
             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
-            maxFreq = Math.max(maxFreq, map.get(s.charAt(right)));
+            count = Math.max(count, map.get(s.charAt(right)));
 
-            if((right - left + 1) - maxFreq > k) {
+            if((right - left + 1) - count > k) {
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
                 left++;
             }
             maxLen = Math.max(maxLen, right - left + 1);
-            right++;
         }
         return maxLen;
     }
