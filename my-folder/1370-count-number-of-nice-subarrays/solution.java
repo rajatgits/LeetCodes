63ea54ed275f9c48589cf7fbfd71b65d7014192f
Rajat Raj <rajatrajemails@gmail.com>
2025-10-14 13:24:25 +0000

    [LeetCode Sync] Runtime - 11 ms (76.40%), Memory - 55.4 MB (27.41%)

--- my-folder/1370-count-number-of-nice-subarrays/solution.java
+++ my-folder/1370-count-number-of-nice-subarrays/solution.java
@@ -1,24 +1,20 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return numOfSubarray(nums, k) - numOfSubarray(nums, k - 1);
+        return numSubarray(nums, k) - numSubarray(nums, k - 1);
     }
 
-    int numOfSubarray(int[] nums, int k) {
+    private int numSubarray(int[] nums, int k) {
         int left = 0;
-        int right = 0;
         int sum = 0;
         int count = 0;
 
-        while(right < nums.length) {
-
+        for(int right = 0; right < nums.length; right++) {
             sum += nums[right] % 2;
-
             while(sum > k && left <= right) {
                 sum -= nums[left] % 2;
                 left++;
             }
             count += (right - left + 1);
-            right++;
         }
         return count;
     }
