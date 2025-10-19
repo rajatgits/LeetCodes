ed1e1bdf61c8948bba64904a6aa6810a31b2df36
Rajat Raj <rajatrajemails@gmail.com>
2025-10-19 08:00:49 +0000

    [LeetCode Sync] Runtime - 18 ms (55.30%), Memory - 45.3 MB (47.32%)

--- my-folder/0076-minimum-window-substring/solution.java
+++ my-folder/0076-minimum-window-substring/solution.java
@@ -1,39 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int start = 0;
+        int left  = 0;
         int count = 0;
         int minLen = Integer.MAX_VALUE;
         int sIndex = -1;
 
         Map<Character, Integer> map = new HashMap<>();
 
-        for(int i=0; i<t.length(); i++) {
+        for(int i = 0; i < t.length(); i++) {
             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        for(int end=0; end<s.length(); end++) {
-            if(map.containsKey(s.charAt(end))) {
-                map.put(s.charAt(end), map.get(s.charAt(end)) - 1);
-                if(map.get(s.charAt(end)) >= 0) {
+        for(int right = 0; right < s.length(); right++) {
+            if(map.containsKey(s.charAt(right))) {
+                map.put(s.charAt(right), map.get(s.charAt(right)) - 1);
+                if(map.get(s.charAt(right)) >= 0) {
                     count++;
-                } 
-            } 
-            
+                }
+            }
+
             while(count == t.length()) {
-                if(end-start+1 < minLen) {
-                    minLen = end-start+1;
-                    sIndex = start;
+                if(right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    sIndex = left;
                 }
 
-                if(map.containsKey(s.charAt(start))) {
-                    map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
-                    if(map.get(s.charAt(start)) > 0) {
+                if(map.containsKey(s.charAt(left))) {
+                    map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                    if(map.get(s.charAt(left)) > 0) {
                         count--;
                     }
                 }
-                start++;
+                left++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(sIndex, sIndex+minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(sIndex, sIndex + minLen);
     }
 }
