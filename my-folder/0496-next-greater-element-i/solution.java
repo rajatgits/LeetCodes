ae546a12b946ea8d38ce501c8566d99d9caba489
Rajat Raj <rajatrajemails@gmail.com>
2025-10-20 12:47:16 +0000

    [LeetCode Sync] Runtime - 3 ms (88.53%), Memory - 43.9 MB (52.87%)

--- my-folder/0496-next-greater-element-i/solution.java
+++ my-folder/0496-next-greater-element-i/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-
         Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[nums1.length];
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int num : nums2) {
             while(!stack.isEmpty() && num > stack.peek()) {
                 map.put(stack.pop(), num);
             }
-            stack.add(num);
+            stack.push(num);
         }
 
         int i = 0;
         for(int num : nums1) {
             ans[i++] = map.getOrDefault(num, -1);
         }
-
         return ans;
     }
 }
