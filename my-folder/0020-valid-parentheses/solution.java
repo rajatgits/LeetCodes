6e0b3524d862c12bdf0c69a305cd7fae762aa856
Rajat Raj <rajatrajemails@gmail.com>
2025-10-20 07:36:48 +0000

    [LeetCode Sync] Runtime - 2 ms (97.37%), Memory - 42 MB (56.47%)

--- my-folder/0020-valid-parentheses/solution.java
+++ my-folder/0020-valid-parentheses/solution.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        Map<Character, Character> map = new HashMap<>();
-
-        map.put(')','(');
-        map.put('}','{');
-        map.put(']','[');
 
         for(char c : s.toCharArray()) {
-            if(map.containsKey(c)) {
-                if(!stack.isEmpty() && stack.peek() == map.get(c)) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
-            } else {
-                stack.push(c);
+            if(c == '(') {
+                stack.push(')');
+            } else if(c == '{') {
+                stack.push('}');
+            } else if(c == '[') {
+                stack.push(']');
+            } else if(stack.isEmpty() || stack.pop() != c) {
+                return false;
             }
         }
         return stack.isEmpty();
