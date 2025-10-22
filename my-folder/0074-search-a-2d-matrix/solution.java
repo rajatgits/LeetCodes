1b097c903a97d5390ca1289c2e5d2200bfe7cf0f
Rajat Raj <rajatrajemails@gmail.com>
2025-10-22 03:46:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (97.35%)

--- my-folder/0074-search-a-2d-matrix/solution.java
+++ my-folder/0074-search-a-2d-matrix/solution.java
@@ -4,17 +4,16 @@ class Solution {
         int right = matrix.length * matrix[0].length - 1;
 
         while(left <= right) {
-            int mid = left+(right-left)/2;
-
-            int row = mid/matrix[0].length;
-            int col = mid%matrix[0].length;
+            int mid = left + (right - left)/2;
+            int row = mid / matrix[0].length;
+            int col = mid % matrix[0].length;
 
             if(matrix[row][col] == target) {
                 return true;
             } else if(matrix[row][col] < target) {
-                left = mid+1;
+                left = mid + 1;
             } else {
-                right = mid-1;
+                right = mid - 1;
             }
         }
         return false;
