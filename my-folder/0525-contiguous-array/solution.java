edfc2923113cfff4f3426c84ad5e0d519ab3f69f
Rajat Raj <rajatrajemails@gmail.com>
2025-10-28 15:22:38 +0000

    [LeetCode Sync] Runtime - 25 ms (18.86%), Memory - 55.4 MB (53.07%)

--- my-folder/0525-contiguous-array/solution.java
+++ my-folder/0525-contiguous-array/solution.java
@@ -1,19 +1,18 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
+        int prefixSum = 0;
         int maxLen = 0;
-
+        Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
 
-        for(int i=0; i<nums.length; i++) {
-            count = count + (nums[i] == 1 ? 1 : -1);
-            if(map.containsKey(count)) {
-                maxLen = Math.max(maxLen, i-map.get(count));
+        for(int i = 0; i < nums.length; i++) {
+            prefixSum += (nums[i] == 1 ? 1 : -1);
+            if(map.containsKey(prefixSum)) {
+                maxLen = Math.max(maxLen, i - map.get(prefixSum));
             } else {
-                map.put(count, i);
+                map.put(prefixSum, i);
             }
         }
         return maxLen;
     }
-} 
+}
