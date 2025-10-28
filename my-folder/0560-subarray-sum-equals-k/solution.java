e74cfddc4cf8108f450a6bc6876db475d9d4bec3
Rajat Raj <rajatrajemails@gmail.com>
2025-10-28 06:40:33 +0000

    [LeetCode Sync] Runtime - 25 ms (46.02%), Memory - 46.4 MB (76.60%)

--- my-folder/0560-subarray-sum-equals-k/solution.java
+++ my-folder/0560-subarray-sum-equals-k/solution.java
@@ -1,17 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int prefixSum = 0;
         int count = 0;
-        int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
 
-        for(int i=0; i<nums.length; i++) {
-            sum += nums[i];
-            
-            if(map.containsKey(sum - k)) {
-                count += map.get(sum - k);
-            } 
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        for(int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            if(map.containsKey(prefixSum - k)) {
+               count += map.get(prefixSum - k);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
     }
