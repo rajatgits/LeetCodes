63c9f9374aa6b031f7ebdb8d915089b61dd00e81
Rajat Raj <rajatrajemails@gmail.com>
2025-10-29 15:39:48 +0000

    [LeetCode Sync] Runtime - 5 ms (91.18%), Memory - 43.5 MB (35.02%)

--- my-folder/0151-reverse-words-in-a-string/solution.java
+++ my-folder/0151-reverse-words-in-a-string/solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] arr = s.strip().replaceAll("\\s{2,}", " ").split(" ");
-        StringBuilder ans = new StringBuilder();
-
-        for(int i = 0; i < arr.length/2; i++) {
-            String temp = arr[i];
-            arr[i] = arr[arr.length - i - 1];
-            arr[arr.length - i - 1] = temp;  
+        String[] str = s.strip().replaceAll("\\s{2,}", " ").split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length - 1; i >= 0; i--) {
+            sb.append(str[i]);
+            sb.append(" ");
         }
-        
-        for(int i = 0; i < arr.length-1; i++) {
-            ans.append(arr[i]+" ");
-        }
-        ans.append(arr[arr.length - 1]);
-
-        return ans.toString();
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
