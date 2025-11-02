7b118279cf4a54e580594db54810b21be9135d4d
Rajat Raj <rajatrajemails@gmail.com>
2025-11-02 14:10:28 +0000

    [LeetCode Sync] Runtime - 2 ms (75.36%), Memory - 55.8 MB (5.16%)

--- my-folder/0169-majority-element/solution.java
+++ my-folder/0169-majority-element/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int ans = nums[0];
+        int num = nums[0];
         int count = 1;
 
         for(int i = 1; i < nums.length; i++) {
             if(count == 0) {
                 count++;
-                ans = nums[i];
-            } else if(ans == nums[i]) {
+                num = nums[i];
+            } else if(num == nums[i]) {
                 count++;
             } else {
                 count--;
             }
         }
-        return ans;
+        return num;
     }
 }
