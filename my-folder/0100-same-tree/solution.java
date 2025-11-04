7f59a5c67eab595ed0d178ef26b5f1f6f3ecf28b
Rajat Raj <rajatrajemails@gmail.com>
2025-11-04 14:05:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.7 MB (6.82%)

--- my-folder/0100-same-tree/solution.java
+++ my-folder/0100-same-tree/solution.java
@@ -15,14 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
-            return true;
+        if(p == null || q == null) {
+            return p == q;
         }
 
-        if(p == null || q == null || p.val != q.val) {
-            return false;
-        }
-        
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
