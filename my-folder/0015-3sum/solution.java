d2f798623a55186fe808135dbd5205fccc8cc163
Rajat Raj <rajatrajemails@gmail.com>
2026-03-03 09:32:08 +0000

    [LeetCode Sync] Runtime - 35 ms (44.15%), Memory - 59 MB (86.22%)

--- my-folder/0015-3sum/solution.java
+++ my-folder/0015-3sum/solution.java
@@ -1,32 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i = 0; i < nums.length && nums[i] <= 0; i++) {
-            if(i == 0 || nums[i] != nums[i - 1]) {
-                twoSum2(nums, i, res);
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
-        }
-        return res;
-    }
 
-    private void twoSum2(int[] nums, int i, List<List<Integer>> res) {
-        int left = i + 1;
-        int right = nums.length - 1;
+            int left = i + 1;
+            int right = nums.length - 1;
 
-        while(left < right) {
-            if(nums[i] + nums[left] + nums[right] < 0) {
-                left++;
-            } else if(nums[i] + nums[left] + nums[right] > 0) {
-                right--;
-            } else {
-                res.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
-                while(left < right && nums[left] == nums[left - 1]) {
+                if(sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
+                    right--;
+                    
+                    while(left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while(left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if(sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
+        return ans;
     }
 }
