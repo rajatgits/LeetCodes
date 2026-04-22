94d5e431296a4548a2af19eb555884d69d159a97
Rajat Raj <rajatrajemails@gmail.com>
2026-04-22 16:42:30 +0000

    [LeetCode Sync] Runtime - 2 ms (99.39%), Memory - 47 MB (57.76%)

--- my-folder/0001-two-sum/solution.java
+++ my-folder/0001-two-sum/solution.java
@@ -3,8 +3,10 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++) {
-            if(map.containsKey(target - nums[i])) {
-                return new int[]{map.get(target - nums[i]), i};
+            int remaining = target - nums[i];
+
+            if(map.containsKey(remaining)) {
+                return new int[]{map.get(remaining), i};
             }
             map.put(nums[i], i);
         }
