696498f109d85bd6c528df01d99b9140483a88e1
Rajat Raj <rajatrajemails@gmail.com>
2026-04-22 16:51:37 +0000

    [LeetCode Sync] Runtime - 1 ms (99.95%), Memory - 77.1 MB (81.28%)

--- my-folder/0053-maximum-subarray/solution.java
+++ my-folder/0053-maximum-subarray/solution.java
@@ -1,7 +1,8 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
         int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
         for(int i = 0; i < nums.length; i++) {
             sum += nums[i];
 
