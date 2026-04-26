694b0194a6231c3136199e32c59d070836fd0daf
Rajat Raj <rajatrajemails@gmail.com>
2026-04-26 07:36:46 +0000

    [LeetCode Sync] Runtime - 20 ms (65.87%), Memory - 45.9 MB (45.88%)

---
+++ my-folder/0018-4sum/solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            for(int j = i + 1; j < nums.length; j++) {
+                if(j > i + 1 && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+
+                int k = j + 1;
+                int l = nums.length - 1;
+                while(k < l) {
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if(sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+                        while(k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
+                    } else if(sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
