0be3b635a77e66c05dc0eb4616b5a42c5a476029
Rajat Raj <rajatrajemails@gmail.com>
2026-05-04 13:39:31 +0000

    [LeetCode Sync] Runtime - 8 ms (17.39%), Memory - 45.4 MB (81.31%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.java
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int maxLen = 0;
         Map<Character, Integer> map = new HashMap<>();
-
+        int maxLen = 0;
+        int left = 0;
         for(int right = 0; right < s.length(); right++) {
             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
-
             while(map.get(s.charAt(right)) > 1) {
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
                 left++;
+                if(s.charAt(left) == 0) {
+                    map.remove(s.charAt(left));
+                }
             }
             maxLen = Math.max(maxLen, right - left + 1);
         }
