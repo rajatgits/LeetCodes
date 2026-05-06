50d14f1127832a134054541fe768d2bbe8529cd9
Rajat Raj <rajatrajemails@gmail.com>
2026-05-06 11:31:00 +0000

    [LeetCode Sync] Runtime - 41 ms (8.12%), Memory - 47.3 MB (5.61%)

--- my-folder/1460-number-of-substrings-containing-all-three-characters/solution.java
+++ my-folder/1460-number-of-substrings-containing-all-three-characters/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public int numberOfSubstrings(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
         int left = 0;
         int count = 0;
 
-        Map<Character, Integer> map = new HashMap<>();
-
         for(int right = 0; right < s.length(); right++) {
             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+
             while(map.size() >= 3) {
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
                 if(map.get(s.charAt(left)) == 0) {
@@ -18,4 +19,4 @@ class Solution {
         }
         return count;
     }
-} 
+}
