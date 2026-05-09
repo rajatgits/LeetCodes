70283dfe661a608e2cfe5baa3a798adee5d176f2
Rajat Raj <rajatrajemails@gmail.com>
2026-05-09 09:52:34 +0000

    [LeetCode Sync] Runtime - 6 ms (45.03%), Memory - 44.2 MB (89.17%)

--- my-folder/0242-valid-anagram/solution.java
+++ my-folder/0242-valid-anagram/solution.java
@@ -5,7 +5,6 @@ class Solution {
         }
 
         int[] hash = new int[26];
-
         for(int i = 0; i < s.length(); i++) {
             hash[s.charAt(i) - 'a']++;
             hash[t.charAt(i) - 'a']--;
