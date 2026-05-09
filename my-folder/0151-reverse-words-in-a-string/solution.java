fdaefa2e1ae1a3494ee1aa827dc0599f23ad356a
Rajat Raj <rajatrajemails@gmail.com>
2026-05-09 05:11:44 +0000

    [LeetCode Sync] Runtime - 5 ms (86.76%), Memory - 45.3 MB (21.19%)

--- my-folder/0151-reverse-words-in-a-string/solution.java
+++ my-folder/0151-reverse-words-in-a-string/solution.java
@@ -1,12 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] str = s.strip().replaceAll("\\s{2,}", " ").split(" ");
+        String[] str = s.trim().replaceAll("\\s{2,}", " ").split(" ");
         StringBuilder sb = new StringBuilder();
+
         for(int i = str.length - 1; i >= 0; i--) {
             sb.append(str[i]);
-            sb.append(" ");
+            if(i != 0) {
+                sb.append(" ");
+            }
         }
-        sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
     }
 }
