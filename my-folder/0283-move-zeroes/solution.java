d0f729aa2d8876d54a1e1ce51bf4ef9fd95ab92b
Rajat Raj <rajatrajemails@gmail.com>
2026-05-12 12:43:18 +0000

    [LeetCode Sync] Runtime - 2 ms (91.71%), Memory - 47.6 MB (86.73%)

--- my-folder/0283-move-zeroes/solution.java
+++ my-folder/0283-move-zeroes/solution.java
@@ -3,10 +3,10 @@ class Solution {
         int i = 0;
         for(int j = 0; j < nums.length; j++) {
             if(nums[j] != 0) {
-               int temp = nums[i];
-               nums[i] = nums[j];
-               nums[j] = temp;
-               i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
             }
         }
     }
