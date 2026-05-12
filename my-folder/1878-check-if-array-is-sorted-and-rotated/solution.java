d6f39fa6e5a8f9937bf3f34812d67e15521e857a
Rajat Raj <rajatrajemails@gmail.com>
2026-05-12 12:29:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (62.98%)

--- my-folder/1878-check-if-array-is-sorted-and-rotated/solution.java
+++ my-folder/1878-check-if-array-is-sorted-and-rotated/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean check(int[] nums) {
-       int irregular = 0;
+        int irregular = 0;
 
-       for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
             if(nums[i] > nums[(i + 1) % nums.length]) {
                 irregular++;
             }
-       }
-       return irregular > 1 ? false : true;
+        }
+        return irregular <= 1 ? true : false;
     }
 }
