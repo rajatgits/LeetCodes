079d9f21b33aa6024f74c7c19943132140b6cc81
Rajat Raj <rajatrajemails@gmail.com>
2026-05-14 11:17:12 +0000

    [LeetCode Sync] Runtime - 3 ms (74.26%), Memory - 52.6 MB (39.55%)

--- my-folder/0229-majority-element-ii/solution.java
+++ my-folder/0229-majority-element-ii/solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
         int cnt1 = 0;
         int cnt2 = 0;
-        int elem1 = Integer.MIN_VALUE;
-        int elem2 = Integer.MIN_VALUE;
+        int element1 = Integer.MIN_VALUE;
+        int element2 = Integer.MIN_VALUE;
 
         for(int i = 0; i < nums.length; i++) {
-            if(cnt1 == 0 && nums[i] != elem2) {
+            if(cnt1 == 0 && nums[i] != element2) {
                 cnt1 = 1;
-                elem1 = nums[i];
-            } else if(cnt2 == 0 && nums[i] != elem1) {
+                element1 = nums[i];
+            } else if(cnt2 == 0 && nums[i] != element1) {
                 cnt2 = 1;
-                elem2 = nums[i];
-            } else if(nums[i] == elem1) {
+                element2 = nums[i];
+            } else if(element1 == nums[i]) {
                 cnt1++;
-            } else if(nums[i] == elem2) {
+            } else if(element2 == nums[i]) {
                 cnt2++;
             } else {
                 cnt1--;
@@ -22,26 +23,29 @@ class Solution {
             }
         }
 
-        List<Integer> ans = new ArrayList<>();
         int count1 = 0;
         int count2 = 0;
 
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == elem1) {
+            if(nums[i] == element1) {
                 count1++;
             }
-            if(nums[i] == elem2) {
+
+            if(nums[i] == element2) {
                 count2++;
             }
         }
 
         int min = (int)(nums.length / 3) + 1;
+
         if(count1 >= min) {
-            ans.add(elem1);
+            ans.add(element1);
         }
+
         if(count2 >= min) {
-            ans.add(elem2);
+            ans.add(element2);
         }
+
         return ans;
     }
 }
