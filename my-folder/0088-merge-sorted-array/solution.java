d9c595beb956268bfe32189495c56c13619a405b
Rajat Raj <rajatrajemails@gmail.com>
2026-05-14 12:54:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.8 MB (59.01%)

--- my-folder/0088-merge-sorted-array/solution.java
+++ my-folder/0088-merge-sorted-array/solution.java
@@ -5,7 +5,7 @@ class Solution {
         int k = m + n - 1;
 
         while(i >= 0 && j >= 0) {
-            if(nums1[i] >= nums2[j]) {
+            if(nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
                 i--;
             } else {
