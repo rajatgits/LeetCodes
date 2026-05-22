bbcc17930549bd8f0f1f333e022ec2cce208b3e3
Rajat Raj <rajatrajemails@gmail.com>
2026-05-22 10:44:43 +0000

    [LeetCode Sync] Runtime - 18 ms (47.13%), Memory - 47.6 MB (31.00%)

--- my-folder/0076-minimum-window-substring/solution.java
+++ my-folder/0076-minimum-window-substring/solution.java
@@ -1,34 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int left  = 0;
-        int count = 0;
-        int minLen = Integer.MAX_VALUE;
-        int sIndex = -1;
-
         Map<Character, Integer> map = new HashMap<>();
+        int sIndex = -1;
+        int minLen = Integer.MAX_VALUE;
+        int count = 0;
 
         for(int i = 0; i < t.length(); i++) {
             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
         }
 
+        int left = 0;
         for(int right = 0; right < s.length(); right++) {
             if(map.containsKey(s.charAt(right))) {
                 map.put(s.charAt(right), map.get(s.charAt(right)) - 1);
                 if(map.get(s.charAt(right)) >= 0) {
-                    count++;
+                    count += 1;
                 }
             }
 
             while(count == t.length()) {
-                if(right - left + 1 < minLen) {
-                    minLen = right - left + 1;
+                if((right - left + 1) < minLen) {
+                    minLen = (right - left + 1);
                     sIndex = left;
                 }
 
                 if(map.containsKey(s.charAt(left))) {
                     map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
                     if(map.get(s.charAt(left)) > 0) {
-                        count--;
+                        count -= 1;
                     }
                 }
                 left++;
