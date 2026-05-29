3c5bea9d4cb016e4501e010afbd05b03e271d09a
Rajat Raj <rajatrajemails@gmail.com>
2026-05-29 08:15:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.8 MB (47.49%)

--- my-folder/0035-search-insert-position/solution.java
+++ my-folder/0035-search-insert-position/solution.java
@@ -1,17 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
+        int low = 0;
+        int high = nums.length - 1;
         int ans = nums.length;
 
-        while(left<=right) {
-            int mid = left+(right-left)/2;
-
+        while(low <= high) {
+            int mid = low + (high - low)/2;
             if(nums[mid] >= target) {
                 ans = mid;
-                right = mid-1;
-            } else{
-                left = mid+1;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
