1f0a95ed4e01eb0470bcadf884b80efe795938f3
Rajat Raj <rajatrajemails@gmail.com>
2026-05-30 13:16:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (55.26%)

--- my-folder/0162-find-peak-element/solution.java
+++ my-folder/0162-find-peak-element/solution.java
@@ -10,19 +10,20 @@ class Solution {
 
         if(nums[nums.length - 1] > nums[nums.length - 2]) {
             return nums.length - 1;
-        }
+        } 
 
-        int left = 1;
-        int right = nums.length - 2;
+        int low = 1;
+        int high = nums.length - 2;
 
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            if(nums[mid] >= nums[mid - 1] && nums[mid] >= nums[mid + 1]) {
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
             } else if(nums[mid] > nums[mid - 1]) {
-                left = mid + 1;
+                low = mid + 1;
             } else {
-                right = mid - 1;
+                high = mid - 1;
             }
         }
         return -1;
