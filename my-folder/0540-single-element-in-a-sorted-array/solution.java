23fa8486dac965c17660288a602a2aea562357e2
Rajat Raj <rajatrajemails@gmail.com>
2026-05-30 12:45:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 52.7 MB (76.51%)

--- my-folder/0540-single-element-in-a-sorted-array/solution.java
+++ my-folder/0540-single-element-in-a-sorted-array/solution.java
@@ -1,30 +1,27 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length == 1) {
+        if(nums.length == 1 || nums[0] != nums[1]) {
             return nums[0];
         }
 
-        if(nums[0] != nums[1]) {
-            return nums[0];
-        }
-
-        if(nums[nums.length - 2] != nums[nums.length - 1]) {
+        if(nums[nums.length - 1] != nums[nums.length - 2]) {
             return nums[nums.length - 1];
         }
 
-        int left = 1;
-        int right = nums.length - 2;
+        int low = 1;
+        int high = nums.length - 2;
 
-        while(left <= right) {
-            int mid = left + (right - left)/2;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
             if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                 return nums[mid];
             }
 
-            if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                left = mid + 1;
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                low = mid + 1;
             } else {
-                right = mid - 1;
+                high = mid - 1;
             }
         }
         return -1;
