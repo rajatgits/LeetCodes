c170d14e28ab0b76539f5817a839748b5233a3fc
Rajat Raj <rajatrajemails@gmail.com>
2026-06-18 10:35:54 +0000

    [LeetCode Sync] Runtime - 2 ms (98.12%), Memory - 43.1 MB (85.08%)

--- my-folder/0020-valid-parentheses/solution.java
+++ my-folder/0020-valid-parentheses/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
-        for(char c : s.toCharArray()) {
+        for(int c : s.toCharArray()) {
             if(c == '(') {
                 stack.push(')');
             } else if(c == '{') {
