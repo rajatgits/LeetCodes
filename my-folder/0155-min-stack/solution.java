722beba2a04730168f74a0cfce5658d3a8bed4fd
Rajat Raj <rajatrajemails@gmail.com>
2026-06-18 11:43:04 +0000

    [LeetCode Sync] Runtime - 27 ms (50.51%), Memory - 104.8 MB (5.07%)

--- my-folder/0155-min-stack/solution.java
+++ my-folder/0155-min-stack/solution.java
@@ -1,11 +1,11 @@
 class MinStack {
     private Node head;
     
-    public void push(int val) {
+    public void push(int value) {
         if(head == null) {
-            head = new Node(val, val, null);
+            head = new Node(value, value, null);
         } else {
-            head = new Node(val, Math.min(val, head.min), head);
+            head = new Node(value, Math.min(value, head.min), head);
         }
     }
     
@@ -37,7 +37,7 @@ class MinStack {
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(val);
+ * obj.push(value);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
