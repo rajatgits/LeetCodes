76279d3dcfd8f39b142055e442fc8fee105341ab
Rajat Raj <rajatrajemails@gmail.com>
2026-06-20 09:48:01 +0000

    [LeetCode Sync] Runtime - 10 ms (79.14%), Memory - 48.4 MB (58.86%)

--- my-folder/0503-next-greater-element-ii/solution.java
+++ my-folder/0503-next-greater-element-ii/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int[] ans = new int[nums.length];
+        Arrays.fill(ans, -1);
 
         Stack<Integer> stack = new Stack<>();
-
-        for(int i = 2*nums.length - 1; i >= 0; i--) {
-            while(!stack.isEmpty() && nums[i % nums.length] >= stack.peek()) {
-                stack.pop();
+        for(int i = 0; i < 2 * nums.length; i++) {
+            while(!stack.isEmpty() && nums[i % nums.length] > nums[stack.peek()]) {
+                int index = stack.pop();
+                ans[index] = nums[i % nums.length];
             }
             if(i < nums.length) {
-                ans[i] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(i % nums.length);
             }
-            stack.push(nums[i % nums.length]);
         }
         return ans;
     }
